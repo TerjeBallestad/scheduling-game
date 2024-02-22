e753f8a66fd243630b02fef11bd9ec23439e4ae2
TerjeBallestad <terj.ba@gmail.com>
2024-02-22 23:14:19 +0100

    ADD: patient needs

---
+++ core/patient/patient_needs.gd
@@ -0,0 +1,10 @@
+class_name PatientNeeds
+
+var food := 100
+var bladder := 100
+var sleep := 100
+var social := 100
+var willpower := 100 # start new tasks
+var body := 100
+var hygiene := 100
+var novelty := 100
